2539f82930a8c817a58a90d17e181fd88a5e2759
Оразбай Уалеш <orazbay@MacBook-Pro-Orazbaj.local>
2017-10-29 00:09:34 +0600

    third commit

--- target/classes/schema.sql
+++
@@ -1,5 +0,0 @@
--- executed by spring boot on H2 DB at test and application start time
-CREATE TABLE IF NOT EXISTS WORDS  (
-   ID               INT  NOT NULL AUTO_INCREMENT  PRIMARY KEY
-  ,value           VARCHAR(255) NOT NULL CONSTRAINT PUBLISHERS_NAME_UC UNIQUE
-);
